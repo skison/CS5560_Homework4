b52c108ab3d59b26c3ee18814b8b82d14e746b17
skison <samuel.j.kison@wmich.edu>
2018-04-22 22:06:03 -0400

    Added extra CORS functionality

--- server/routes.ts
+++ server/routes.ts
@@ -36,6 +36,7 @@ export default function setRoutes(app) {
   //allow cross-site api requests
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
